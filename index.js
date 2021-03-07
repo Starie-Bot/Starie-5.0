4df3a2848dba474e4953406ddb779ec3b04d5160
Matthew Elswick <matthe815@gmail.com>
2021-03-07 17:08:52 -0500

    Index Clean-up Cleaned the Index.JS file; adding comments and some UX code. Added a new "error" log.

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const { Client } = require('discord.js')
+const Logger = new (require('./src/Logging/Logger'))()
 const config = require('./config/config.json')
-const client = new Client();
+const client = new Client()
 
 new (require('./src/SlashCommands/SlashCommands'))(client, config)
 
 const OWNERS = []
 
 client.on('ready', async _ => {
-  for (const ownerID of config.OWNERS) {
-    const owner = await client.users.fetch(ownerID)
+  for (const ownerID of config.OWNERS) { // Loop through the owners present in the config file.
+    const owner = await client.users.fetch(ownerID) // Fetch the user-object using the ID.
 
+    // If it fails to get an object, just ignore it completely.
     if (!owner) {
+      Logger.Error(`Could not load an owner with the ID of [${ownerID}]`)
       continue
     }
 
-    OWNERS.push(owner)
+    OWNERS.push(owner) // Push the newly found object into an array.
   }
 })
 
-client.login(config.TOKEN)
+client.login(config.TOKEN) // Log-in to Discord
--- src/Logging/Logger.js
+++ src/Logging/Logger.js
@@ -13,6 +13,15 @@ class Logger {
   Print (message, type = LogTypes.NETCALL) {
     if (this.enabledTypes.includes(type)) { return console.log(`[${new Date().getHours()}:${new Date().getMinutes()}] [${type}] ${message}`) }
   }
+
+  /**
+     * Print an error message into the console.
+     * @param {String} message
+     * @param {LogTypes} type
+     */
+  Error (message) {
+    return console.log(`[ERROR] [${new Date().getHours()}:${new Date().getMinutes()}] [${type}] ${message}`)
+  }
 }
 
 module.exports = Logger
